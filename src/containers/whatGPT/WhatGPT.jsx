2467338e7f62c34ae62e6a4033fa0ff103be6b7f
Ender Dagdelen <enderdagdelen@hotmail.com>
2024-01-31 21:56:15 +0300

    Version 1

---
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './whatGPT.css';
+
+const WhatGPT = () => {
+  return (
+    <div>
+        <h1>WHATGPT</h1>
+        
+    </div>
+  )
+}
+
+export default WhatGPT
